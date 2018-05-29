a14f288e8570b555a2fefd5f9f6206a020cd839b
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-05-29 11:48:59 +0530

    Arithmatic operations in java

---
+++ MathematicalOperations.java
@@ -0,0 +1,30 @@
+class MathematicalOperations
+{
+	public static void main(String args[])
+	{
+		System.out.println("ARITHMATIC OPERATORS");
+		int x = 12;
+		int y = 14;
+		int z = x+y;
+		
+		System.out.println("Addition of 2 integers:"+z);
+		
+		int a = x*y;
+		System.out.println("Multiplication of 2 integers:"+a);
+		
+		long b = x/y;
+		System.out.println("Divition of 2 integers:"+b);
+		
+		int c = y%x;
+		System.out.println("Modulous of 2 integers:"+c);
+		
+        System.out.println("Bitwise Operators");
+
+
+		
+		
+	}
+	
+	
+	
+}
